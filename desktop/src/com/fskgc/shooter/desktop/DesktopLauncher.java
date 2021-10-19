326a445d7716a59bd09fe68895d7d64616bf5ff0
Kirill <kirbo06@mail.ru>
2021-10-19 18:55:19 +0200

    create a base project template

---
+++ desktop/src/com/fskgc/shooter/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.fskgc.shooter.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.fskgc.shooter.Shooter;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Shooter(), config);
+	}
+}
